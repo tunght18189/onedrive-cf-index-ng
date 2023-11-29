479ff0f9f9c50260f1de8efd8ddfb391e25f4eb6
tunght18189 <152156615+tunght18189@users.noreply.github.com>
2023-11-29 10:49:00 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Tung_GKFV's OneDrive",
+  title: "Tung_E5's OneDrive",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: '/',
